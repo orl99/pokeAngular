555e2a7464dd303ed518c30ebbdbafa65a8d68e1
Orlando Garcia <orlando.garcia.64@hotmail.com>
2019-05-06 10:47:28 -0500

    Second Commit
    In this commit some files and structure have been added.
    
    Commited with a lot of ❤☕💪 for all the entire 🌌

---
+++ src/app/components/poke-nav/poke-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-nav',
+  templateUrl: './poke-nav.component.html',
+  styleUrls: ['./poke-nav.component.scss']
+})
+export class PokeNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
